231ca7910268cacadbb9e8d3b0b09fe0c440e3b7
Lukasz Bieganski <l8b@qad.com>
2015-10-13 09:33:46 +0000

    Changes from Screen Builder

---
+++ AvengersApp/components/homeView/index.js
@@ -0,0 +1,82 @@
+'use strict';
+
+app.homeView = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_homeView
+// END_CUSTOM_CODE_homeView
+(function(parent) {
+    var dataProvider = app.data.defaultProvider,
+        flattenLocationProperties = function(dataItem) {
+            var propName, propValue,
+                isLocation = function(value) {
+                    return propValue && typeof propValue === 'object' &&
+                        propValue.longitude && propValue.latitude;
+                };
+
+            for (propName in dataItem) {
+                if (dataItem.hasOwnProperty(propName)) {
+                    propValue = dataItem[propName];
+                    if (isLocation(propValue)) {
+                        // Location type property
+                        dataItem[propName] =
+                            kendo.format('Latitude: {0}, Longitude: {1}',
+                                propValue.latitude, propValue.longitude);
+                    }
+                }
+            }
+        },
+        dataSourceOptions = {
+            type: 'everlive',
+            transport: {
+                typeName: 'Activities',
+                dataProvider: dataProvider
+            },
+
+            change: function(e) {
+                var data = this.data();
+                for (var i = 0; i < data.length; i++) {
+                    var dataItem = data[i];
+
+                    flattenLocationProperties(dataItem);
+                }
+            },
+            schema: {
+                model: {
+                    fields: {
+                        'Text': {
+                            field: 'Text',
+                            defaultValue: ''
+                        },
+                    }
+                }
+            },
+            serverSorting: true,
+            serverPaging: true,
+            pageSize: 50
+        },
+        dataSource = new kendo.data.DataSource(dataSourceOptions),
+        homeViewModel = kendo.observable({
+            dataSource: dataSource,
+            itemClick: function(e) {
+                app.mobileApp.navigate('#components/homeView/details.html?uid=' + e.dataItem.uid);
+            },
+            detailsShow: function(e) {
+                var item = e.view.params.uid,
+                    dataSource = homeViewModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(item);
+                if (!itemModel.Likes) {
+                    itemModel.Likes = String.fromCharCode(160);
+                }
+                homeViewModel.set('currentItem', itemModel);
+            },
+            currentItem: null
+        });
+
+    parent.set('homeViewModel', homeViewModel);
+})(app.homeView);
+
+// START_CUSTOM_CODE_homeViewModel
+// END_CUSTOM_CODE_homeViewModel
