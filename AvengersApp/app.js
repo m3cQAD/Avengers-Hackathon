429cfe25fe35da796263cccb8697dda39617401e
Grzegorz Baltaza <gkb@qad.com>
2015-10-13 10:17:34 +0000

    Changes from Screen Builder

--- AvengersApp/app.js
+++ AvengersApp/app.js
@@ -15,7 +15,7 @@
                 // and feel of the operating system
                 skin: 'flat',
                 // the application needs to know which view to load first
-                initial: 'components/authenticationView/view.html',
+                initial: 'components/homeView/view.html',
                 statusBarStyle: 'black-translucent'
             });
         });
